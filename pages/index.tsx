7f1278680466131169b2ee5359e29188ec195494
bennisan86 <info@bennisan.be>
2023-09-22 18:01:13 +0200

    Init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,34 @@
-import { Button, Flex, Text, VStack } from '@chakra-ui/react';
+import { Button, Flex, Heading, Text, VStack } from '@chakra-ui/react';
 import NextLink from 'next/link';
 
 export default function Home(): JSX.Element {
   return (
-    <VStack as="main" pt={20}>
-      <Text fontSize="2xl" variant="heading">
-        CharacterSheets
-      </Text>
-      <Text pt={4}>
-        Build a character through your work as a web3 mercenary. Slay moloch,
-        earn XP.
-      </Text>
-      <Flex margin="60px auto 0" justify="space-between" width="280px" gap="4">
-        <Button as={NextLink} href="/my-games">
-          My Games
-        </Button>
-        <Button as={NextLink} href="/all-games">
-          All Games
-        </Button>
-      </Flex>
-    </VStack>
+    <Flex
+      direction={'column'}
+      pt={'10vh'}
+    >
+      {/* <Heading size='2xl'>Eternalize your journey</Heading>
+         <Text>
+          Build a character through your work as a web3 mercenary. Slay moloch,
+          earn XP.
+        </Text> */}
+    </Flex>
+    // <VStack as="main" pt={20}>
+    //   <Text fontSize="2xl" variant="heading">
+    //     CharacterSheets
+    //   </Text>
+    //   <Text pt={4}>
+    //     Build a character through your work as a web3 mercenary. Slay moloch,
+    //     earn XP.
+    //   </Text>
+    //   <Flex margin="60px auto 0" justify="space-between" width="280px" gap="4">
+    //     <Button as={NextLink} href="/my-games">
+    //       My Games
+    //     </Button>
+    //     <Button as={NextLink} href="/all-games">
+    //       All Games
+    //     </Button>
+    //   </Flex>
+    // </VStack>
   );
 }
