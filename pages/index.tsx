90a75cb83a64a84dda126a854b9aed90e9aec771
bennisan86 <info@bennisan.be>
2023-10-11 12:06:20 +0200

    sync

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,12 +8,12 @@ export default function Home(): JSX.Element {
   const { createGameModal } = useGamesContext();
 
   return (
-    <Flex direction="column" pt="110px" px="10vw">
-      <Heading fontSize="80px" lineHeight="70px" fontWeight={"regular"} textTransform={"capitalize"} maxW={"720px"}>Eternalize your journey</Heading>
-      <Text fontSize={{ base: 'lg', lg: 'lg' }} maxW="590px" mt="10" w="full">
+    <Flex direction="column" pt="100px" px="10vw">
+      <Heading fontSize="66px" lineHeight="56px" fontWeight={"regular"} textTransform={"capitalize"} maxW={"720px"}>Eternalize your journey</Heading>
+      <Text fontSize={{ base: 'lg', lg: 'md' }} maxW="520px" mt="10" w="full">
         Build your character as a live chronicle of your actions. Collect items, earn XP and level up classes to register your  growth on-chain.
       </Text>
-      <Flex direction={{ base: 'column', lg: 'row' }} mt="14">
+      <Flex direction={{ base: 'column', lg: 'row' }} mt="10">
         <Button
           as={NextLink}
           href="/all-games"
