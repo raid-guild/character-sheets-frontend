85682efa2b08d67a64ccf7763547125b1921eec8
ECWireless <elliott@coopallc.com>
2023-11-14 14:35:00 -0700

    Log URI in uriToHttp helper

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -22,6 +22,8 @@ import {
  * @param uri to convert to fetch-able http url
  */
 export const uriToHttp = (uri: string): string[] => {
+  // eslint-disable-next-line no-console
+  console.log('Test', uri);
   try {
     const protocol = uri.split(':')[0].toLowerCase();
     switch (protocol) {
