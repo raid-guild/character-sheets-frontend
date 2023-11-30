8061d29c03ba69b0de32fa679f85720088f0a5b0
dan13ram <dan13ram@gmail.com>
2023-11-30 18:07:49 +0530

    clean up item tag

--- components/ItemTag.tsx
+++ components/ItemTag.tsx
@@ -11,60 +11,29 @@ import { useMemo } from 'react';
 
 import { useGame } from '@/contexts/GameContext';
 import { PlayerActions, useItemActions } from '@/contexts/ItemActionsContext';
+import { useIsConnectedAndMounted } from '@/hooks/useIsConnectedAndMounted';
 import { Item } from '@/utils/types';
-
-const fontSizeMap = {
-  sm: 'xs',
-  md: 'sm',
-  lg: 'md',
-};
-
-const paddingMap = {
-  sm: 2,
-  md: 4,
-  lg: 6,
-};
-
-const spacingMap = {
-  sm: 2,
-  md: 3,
-  lg: 4,
-};
-
-type Size = 'sm' | 'md' | 'lg';
 
 type ItemTagProps = {
   holderId?: string;
   item: Item;
-  size?: Size;
 };
 
-export const ItemTag: React.FC<ItemTagProps> = ({
-  size = 'md',
-  item,
-  holderId,
-}) => {
-  const { fontSize, padding, spacing } = useMemo(
-    () => ({
-      fontSize: fontSizeMap[size],
-      padding: paddingMap[size],
-      spacing: spacingMap[size],
-    }),
-    [size],
-  );
-
+export const ItemTag: React.FC<ItemTagProps> = ({ item, holderId }) => {
   const { character } = useGame();
 
   const { itemId, name, image, amount } = item;
 
-  const showActions = useMemo(() => {
-    return (
-      size != 'sm' &&
+  const isConnectedAndMounted = useIsConnectedAndMounted();
+
+  const isHolder = useMemo(
+    () =>
       !!holderId &&
       !!character &&
-      holderId === character.characterId
-    );
-  }, [size, character, holderId]);
+      holderId === character.characterId &&
+      isConnectedAndMounted,
+    [character, holderId, isConnectedAndMounted],
+  );
 
   const isEquipped = useMemo(() => {
     return (
@@ -78,58 +47,60 @@ export const ItemTag: React.FC<ItemTagProps> = ({
   const { openActionModal, selectItem } = useItemActions();
 
   return (
-    <VStack
-      p={padding}
-      spacing={spacing}
-      w="auto"
-      h="100%"
-      justify="space-between"
-      bg="whiteAlpha.100"
-      borderRadius="lg"
-      pos="relative"
-    >
-      {isEquipped && (
-        <Flex
-          borderRadius="50%"
-          pos="absolute"
-          w="1.675rem"
-          h="1.675rem"
-          top={2}
-          right={2}
-          bg="dark"
-          justify="center"
-          align="center"
-        >
-          <CheckIcon color="white" w="0.875rem" />
-        </Flex>
-      )}
-      <AspectRatio ratio={1} w="100%">
-        <Image
-          alt={name}
-          w="100%"
-          style={{
-            objectFit: 'contain',
-          }}
-          src={image}
-        />
-      </AspectRatio>
-      <VStack w="100%">
-        <Text textAlign="center" fontSize={fontSize}>
+    <VStack spacing={2} w="100%" h="100%">
+      <VStack
+        p={4}
+        spacing={3}
+        w="100%"
+        h="100%"
+        justify="space-between"
+        bg="whiteAlpha.100"
+        borderRadius="lg"
+        pos="relative"
+      >
+        {isEquipped && (
+          <Flex
+            borderRadius="50%"
+            pos="absolute"
+            w="1.675rem"
+            h="1.675rem"
+            top={2}
+            right={2}
+            bg="dark"
+            justify="center"
+            align="center"
+          >
+            <CheckIcon color="white" w="0.875rem" />
+          </Flex>
+        )}
+        <AspectRatio ratio={1} w="100%">
+          <Image
+            alt={name}
+            w="100%"
+            style={{
+              objectFit: 'contain',
+            }}
+            src={image}
+          />
+        </AspectRatio>
+        <Text textAlign="center" fontSize="sm">
           {name} ({amount.toString()})
         </Text>
-        {showActions && !!character && (
-          <Button
-            onClick={() => {
-              selectItem(item);
-              openActionModal(PlayerActions.EQUIP_ITEM);
-            }}
-            size="sm"
-            w="100%"
-          >
-            {isEquipped ? 'Unequip' : 'Equip'}
-          </Button>
-        )}
       </VStack>
+
+      {isHolder && (
+        <Button
+          onClick={() => {
+            selectItem(item);
+            openActionModal(PlayerActions.EQUIP_ITEM);
+          }}
+          size="sm"
+          w="100%"
+          variant="solid"
+        >
+          {isEquipped ? 'Unequip' : 'Equip'}
+        </Button>
+      )}
     </VStack>
   );
 };
