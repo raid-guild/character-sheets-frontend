e333ea73f2d8a27a02a36bc8965913ab63e1883f
ECWireless <elliott@coopallc.com>
2023-12-04 20:09:41 -0700

    Try switching cloudflare to ipfs.io

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -18,7 +18,7 @@ import {
   Metadata,
 } from './types';
 
-const IPFS_GATEWAYS = ['https://ipfs.io', 'https://cloudflare-ipfs.com'];
+const IPFS_GATEWAYS = ['https://cloudflare-ipfs.com', 'https://ipfs.io'];
 
 /**
  * Given a URI that may be ipfs, ipns, http, https, ar, or data protocol, return the fetch-able http(s) URLs for the same content
