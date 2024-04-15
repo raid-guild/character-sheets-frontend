25929b02daa0f701182db5f51534e06480a667b4
dan13ram <dan13ram@gmail.com>
2024-04-15 18:46:37 +0530

    added support for new networks

--- lib/web3/constants.ts
+++ lib/web3/constants.ts
@@ -1,5 +1,12 @@
 import { Chain } from '@rainbow-me/rainbowkit';
-import { gnosis, mainnet, sepolia } from 'wagmi/chains';
+import {
+  base,
+  gnosis,
+  mainnet,
+  optimism,
+  polygon,
+  sepolia,
+} from 'wagmi/chains';
 
 import { ENVIRONMENT } from '@/utils/constants';
 
@@ -12,55 +19,88 @@ export const WALLET_CONNECT_PROJECT_ID: string =
 export const EXPLORER_URLS: { [key: number]: string } = {
   [gnosis.id]: 'https://gnosisscan.io',
   [sepolia.id]: 'https://sepolia.etherscan.io',
+  [base.id]: 'https://basescan.org',
+  [polygon.id]: 'https://polygonscan.com',
+  [optimism.id]: 'https://optimistic.etherscan.io',
 };
 
 export const SUBGRAPH_URLS: { [key: number]: string } = {
   [gnosis.id]:
-    'https://api.thegraph.com/subgraphs/name/dan13ram/character-sheets-gnosis',
+    'https://api.studio.thegraph.com/query/71457/character-sheets-gnosis/version/latest',
   [sepolia.id]:
     'https://api.studio.thegraph.com/query/71457/character-sheets-sepolia/version/latest',
+  [base.id]:
+    'https://api.studio.thegraph.com/query/71457/character-sheets-base/version/latest',
+  [polygon.id]:
+    'https://api.studio.thegraph.com/query/71457/character-sheets-polygon/version/latest',
+  [optimism.id]:
+    'https://api.studio.thegraph.com/query/71457/character-sheets-optimism/version/latest',
 };
 
 export const RPC_URLS: { [key: number]: string } = {
   [gnosis.id]: 'https://rpc.gnosis.gateway.fm',
   [sepolia.id]: `https://sepolia.infura.io/v3/${INFURA_KEY}`,
   [mainnet.id]: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
+  [polygon.id]: `https://polygon-mainnet.infura.io/v3/${INFURA_KEY}`,
+  [optimism.id]: `https://optimism-mainnet.infura.io/v3/${INFURA_KEY}`,
+  [base.id]: 'https://mainnet.base.org',
 };
 
 export const SERVER_RPC_URLS: { [key: number]: string } = {
   [gnosis.id]: 'https://rpc.gnosis.gateway.fm',
   [sepolia.id]: `https://sepolia.infura.io/v3/${SERVER_INFURA_KEY}`,
   [mainnet.id]: `https://mainnet.infura.io/v3/${SERVER_INFURA_KEY}`,
+  [polygon.id]: `https://polygon-mainnet.infura.io/v3/${SERVER_INFURA_KEY}`,
+  [optimism.id]: `https://optimism-mainnet.infura.io/v3/${SERVER_INFURA_KEY}`,
+  [base.id]: 'https://mainnet.base.org',
 };
 
 export const CHAINS: { [key: number]: Chain } = {
   [gnosis.id]: gnosis,
   [sepolia.id]: sepolia,
   [mainnet.id]: mainnet,
+  [polygon.id]: polygon,
+  [optimism.id]: optimism,
+  [base.id]: base,
 };
 
 export const CHAIN_LABEL_TO_ID: { [key: string]: number } = {
   gnosis: gnosis.id,
   sepolia: sepolia.id,
+  mainnet: mainnet.id,
+  polygon: polygon.id,
+  optimism: optimism.id,
+  base: base.id,
 };
 
 export const CHAIN_ID_TO_IMAGE: { [key: number]: string } = {
   [gnosis.id]: '/images/gnosis.svg',
   [sepolia.id]: '/images/ethereum.svg',
   [mainnet.id]: '/images/ethereum.svg',
-  420: '/images/optimism.svg',
-  137: '/images/polygon.svg',
+  [optimism.id]: '/images/optimism.svg',
+  [polygon.id]: '/images/polygon.svg',
+  [base.id]: '/images/base.svg',
   42161: '/images/arbitrum.svg',
 };
 
 export const CHAIN_ID_TO_LABEL: { [key: number]: string } = {
   [gnosis.id]: 'gnosis',
   [sepolia.id]: 'sepolia',
+  [mainnet.id]: 'mainnet',
+  [polygon.id]: 'polygon',
+  [optimism.id]: 'optimism',
+  [base.id]: 'base',
 };
 
 type _chains = readonly [Chain, ...Chain[]];
 
-const ALL_SUPPORTED_CHAINS: _chains = [gnosis, sepolia];
+const ALL_SUPPORTED_CHAINS: _chains = [
+  gnosis,
+  sepolia,
+  polygon,
+  optimism,
+  base,
+];
 
 export const SUPPORTED_CHAINS: _chains = (() => {
   switch (ENVIRONMENT) {
